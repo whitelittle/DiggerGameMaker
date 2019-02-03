dd09a1456a137c519f6c1d336af232b2811ad410
略略略 <843402802@qq.com>
2019-02-03 15:42:12 +0800

    游戏管理器上传
    作者：略略略
    初步构思关卡UI管理

---
+++ Unity_Project/DiggerJamGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public int CurLevel = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LoadNextLoad()
+    {
+        CurLevel++;
+        SceneManager.LoadScene("Level_" + CurLevel);
+    }
+}
