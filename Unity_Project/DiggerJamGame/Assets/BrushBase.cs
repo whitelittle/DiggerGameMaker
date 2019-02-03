1b02bc2d8d164d25498e7d94ca9e90a68549a5ed
GameFantasy <561547818@qq.com>
2019-02-03 19:12:49 +0800

    添加通过绘线实现球体运动的功能
    1.添加通过绘线实现球体运动的功能
    2.稍微修改了下烘焙的的一些配置

--- Unity_Project/DiggerJamGame/Assets/BrushBase.cs
+++ Unity_Project/DiggerJamGame/Assets/BrushBase.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.SceneManagement;
 public class BrushBase : MonoBehaviour
 {
+    public GameObject obj;
     //LineRenderer  
     private LineRenderer lineRenderer;
     //定义一个Vector3,用来存储鼠标点击的位置  
@@ -16,7 +17,10 @@ public class BrushBase : MonoBehaviour
     private Vector3 startPosition;
     //端点数  
     private Vector3 endPosition;
+    //球体物理
+    Rigidbody rig;
 
+    private bool gameStatus = true;
     void Start()
     {
         //添加LineRenderer组件  
@@ -28,19 +32,22 @@ public class BrushBase : MonoBehaviour
         //设置宽度  
         lineRenderer.SetWidth(0.05f, 0.05f);
 
-    }
 
+
+
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             //记录起点
             startPosition = Input.mousePosition;
+           
         }
         //获取LineRenderer组件  
         lineRenderer = GetComponent<LineRenderer>();
         //鼠标左击  
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)&& gameStatus)
         {
             //将鼠标点击的屏幕坐标转换为世界坐标，然后存储到position中  
             position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
@@ -53,15 +60,23 @@ public class BrushBase : MonoBehaviour
             index++;
 
         }
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0)&& gameStatus)
         {
             //清空上一条线的数据
             lineRenderer = new LineRenderer();
             position = new Vector3();
             index = 0;
             LengthOfLineRenderer = 0;
-            //记录起点
+            //记录终点
             endPosition = Input.mousePosition;
+            //添加刚体
+            rig = obj.AddComponent<Rigidbody>();
+            //终点减去起点获得向量
+            Vector3 offset = endPosition - startPosition;
+            //施加力
+            rig.AddForce(offset*2);
+            //修改游戏状态
+            gameStatus = false;
 
         }
 
@@ -78,6 +93,21 @@ public class BrushBase : MonoBehaviour
         GUILayout.Label("终点位置：(" + endPosition.x + "," + endPosition.y + ")");
         GUILayout.Label("直角边交点：(" + endPosition.x + "," + startPosition.y + ")");
 
+
+        Vector3 offset = startPosition - endPosition;
+        float distance = offset.magnitude;
+        GUILayout.Label("距离："+ distance);
+        GUILayout.Label("状态：" + gameStatus);
+        if (!gameStatus )
+        {
+            if (GUI.Button(new Rect(50, 250, 200, 30), "重置"))
+            {
+                //重置场景
+                SceneManager.LoadScene("Test_Brush");
+
+            }
+        }
+ 
     }
 
 
