3fa79c5153f6e6e87b4b2df6ac9462c770b8e8c8
略略略 <843402802@qq.com>
2019-02-06 01:14:45 +0800

    根据关卡数实例化UI

--- Unity_Project/DiggerJamGame/Assets/Script/GameManager.cs
+++ Unity_Project/DiggerJamGame/Assets/Script/GameManager.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.IO;
 
-public class GameManager : MonoBehaviour {
+public static class GameManager{
 
-    public static GameManager Instance;
-
-    public int CurLevel = 0;
-
-    private void Awake()
+    public static int maxLevelIndex = 1;
+    static string GameDataPath =Application.dataPath +"Data/GameData.txt";
+    public static void LoadLevel(int levelIndex)
     {
-        Instance = this;
+        SceneManager.LoadScene(levelIndex);
+
+        if (levelIndex > maxLevelIndex)
+        {
+            maxLevelIndex = levelIndex;
+        }
     }
 
-    public void LoadNextLoad(int levelIndex)
+    public static void SaveGameData()
     {
-        SceneManager.LoadScene("Level_" + levelIndex);
-        CurLevel = levelIndex;
+
+    }
+
+    public static void ReadFile(string FileName)
+    {
+        string[] strs = File.ReadAllLines(FileName);//读取文件的所有行，并将数据读取到定义好的字符数组strs中，一行存一个单元
+        for (int i = 0; i < strs.Length; i++)
+        {
+        }
+    }
+
+    public static void WriteFile()
+    {
+        
     }
 }
---
+++ Unity_Project/DiggerJamGame/Assets/Script/LoadLevel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+    int levelCount;
+
+    //放置关卡按钮的容器
+    [SerializeField] Transform btnContent;
+    //按钮预制体
+    [SerializeField] GameObject btnPrefab;
+
+    private void Awake()
+    {
+        //关卡数=总场景数-Main场景
+        levelCount = SceneManager.sceneCountInBuildSettings - 1;
+    }
+
+    private void Start()
+    {
+        SpawnButton();
+    }
+
+    public void SpawnButton()
+    {
+        //索引0是关卡选择场景
+        for (int i = 1; i < levelCount; i++)
+        {
+            //根据关卡数实例化关卡按钮
+            GameObject btnGo = Instantiate(btnPrefab, btnContent);
+
+            //开辟新内存 防止onClick监听至同一个参数
+            int index = i;
+
+            //获取按钮监听点击事件
+            Button btn = btnGo.GetComponent<Button>();
+            btn.onClick.AddListener(() => GameManager.LoadLevel(index));
+
+            //设置按钮名称
+            Text text = btnGo.GetComponentInChildren<Text>();
+            text.text = "关卡" + index;
+
+            if (index > GameManager.maxLevelIndex)
+            {
+                btn.interactable = false;
+            }
+        }
+    }
+}
