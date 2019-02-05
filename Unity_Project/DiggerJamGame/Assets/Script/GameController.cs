a024f9718a60e0d3d5c514f9c9a16937a1457a1f
GameFantasy <561547818@qq.com>
2019-02-05 15:36:15 +0800

    修改目录
    1.更名lueBrush为GameController
    2.修改部分目录名称

--- Unity_Project/DiggerJamGame/Assets/Script/GameController.cs
+++ Unity_Project/DiggerJamGame/Assets/Script/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-public class LueBrush : MonoBehaviour
+public class GameController : MonoBehaviour
 {
 
 
